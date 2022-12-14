c7cafbf12b15a74909663e87ea98cb5fa119a3e4
Thomas <tfnielson.se@gmail.com>
2022-12-14 15:56:24 -0700

    commit tree bf4b65ec0dcdff66212edb3c38170914d20f17f5

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById(`dodger`);
+
+dodger.style.bottom = `0px`;
+dodger.style.background = `yellow`;
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace(`px`, ``);
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left - 1}`;
+    }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace(`px`, ``);
+    const left = parseInt(rightNumbers, 10);
+    
+    if (left > 360) {
+        dodger.style.left = `${left - 1}`;
+    }
+}
+
+document.addEventListener(`keydown`, function(e) {
+    if (e.key === `ArrowLeft`) {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener(`keydown`, function(e) {
+    if (e.key === `ArrowRight`) {
+        moveDodgerLeft();
+    }
+});
